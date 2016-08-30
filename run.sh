64ea84d34abe74f61f2d5b50d38766e871c4ec59
fengqi <lyf362345@gmail.com>
2016-08-30 08:59:02 +0000

    去掉 SSH

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mkdir -p /var/run/sshd && /usr/sbin/sshd && /opt/ngrok/ngrokd -httpAddr=":80" -httpsAddr=":443" -tunnelAddr=":4443" -tlsCrt="/opt/ngrok/ssl.crt" -tlsKey="/opt/ngrok/ssl.key" -domain="fengqi.tk" -log-level="WARNING"
+/opt/ngrok/ngrokd -httpAddr=":80" -httpsAddr=":443" -tunnelAddr=":4443" -tlsCrt="/opt/ngrok/ssl.crt" -tlsKey="/opt/ngrok/ssl.key" -domain="fengqi.tk" -log-level="WARNING"
 
