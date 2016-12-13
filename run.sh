4eecdb4bd923b923bbb5354bce60a13b7f638f42
fengqi <lyf362345@gmail.com>
2016-12-13 15:48:41 +0800

    小调整

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-/opt/ngrok/ngrokd -httpAddr=":80" -httpsAddr=":443" -tunnelAddr=":4443" -tlsCrt="/opt/ngrok/ssl.crt" -tlsKey="/opt/ngrok/ssl.key" -domain="fengqi.ml" -log-level="WARNING"
+/opt/ngrok/ngrokd \
+    -httpAddr=":80" \
+    -httpsAddr=":443" \
+    -tunnelAddr=":4443" \
+    -tlsCrt="/opt/ngrok/ssl.crt" \
+    -tlsKey="/opt/ngrok/ssl.key" \
+    -domain="fengqi.ml" \
+    -log-level="WARNING"
 
